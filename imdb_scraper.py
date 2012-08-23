99381d792c567a7f0b9e6735995194d85d61233f
Eoin H <UltimateHurl@gmail.com>
2012-08-23 23:14:42 +0100

    Fixed a bug when IMDB redirected from search to the film's page

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -1,5 +1,6 @@
 #!/bin/python
 import os,re,urllib,urllib2
+import HTMLParser
 from BeautifulSoup import BeautifulSoup as Soup
 
 def getFilmPage(item):
@@ -7,8 +8,16 @@ def getFilmPage(item):
 	#<p><b>Popular Titles</b> (Displaying 1 Result)<table><tr> <td valign="top"><a href="/title/tt1592873/" onClick="(new Image()).src='/rg/find-tiny-photo-1/title_popular/images/b.gif?link=/title/tt1592873/';"><img src="http://ia.media-imdb.com/images/M/MV5BMTA0MjI5ODA3MjReQTJeQWpwZ15BbWU3MDI1NTE3Njc@._V1._SY30_SX23_.jpg" width="23" height="32" border="0"></a>&nbsp;</td><td align="right" valign="top"><img src="/images/b.gif" width="1" height="6"><br>1.</td><td valign="top"><img src="/images/b.gif" width="1" height="6"><br><a href="/title/tt1592873/" onclick="(new Image()).src='/rg/find-title-1/title_popular/images/b.gif?link=/title/tt1592873/';">LOL</a> (2012)                         <p class="find-aka">aka  "LOL USA" - France <em>(imdb display title)</em></p>   
 	#http://www.imdb.com/title/tt1592873/
 	try:
-		page = urllib2.urlopen("http://www.imdb.com/find?q="+str(urllib2.quote(item))+"&s=all")
+		film = item.replace(' III',' 3')
+		film = film.replace(' II',' 2')
+		film = film.replace(' I',' 1')
+		film = film.replace(' IV',' 4')
+		film = film.replace(' VI',' 5')
+		film = urllib2.quote(film)
+		page = urllib2.urlopen("http://www.imdb.com/find?q="+str(film)+"&s=all")
 		soup = Soup(page)
+		if soup.find("h1", {"itemprop":"name"}) != None:
+			return soup
 		result = soup.find("a", {"href":re.compile('^/title/')})
 		page = urllib2.urlopen("http://www.imdb.com"+str(result['href']))
 		soup = Soup(page)
@@ -39,10 +48,31 @@ def getFullTitle(imdbpage):  #The Expendables 2 (2012) 7.7
 	#<span itemprop="ratingValue">7.7</span>
 	info = imdbpage.find("h1", {"itemprop":"name"})
 	title = str(info)
-	title = title[title.find('>')+1:title.find('<span')-1].strip()
-	year = info.find("a")
-	rating = imdbpage.find("span", {"itemprop":"ratingValue"}).string
-	return title + " ("+year.contents[0]+") "+ rating
+	if title != 'None':
+		title = title[title.find('>')+1:title.find('<span')-1].strip()
+		title = urllib2.unquote(title)
+		try:
+			year = info.find("a")
+			year = year.contents[0]
+		except AttributeError:
+			year = title[title.rfind('('):]
+		try:
+			rating = imdbpage.find("span", {"itemprop":"ratingValue"}).string
+		except AttributeError:
+			rating = ""
+	else:
+		title = str(imdbpage)
+		fo = open('r.html','w')
+		fo.write(title)
+		title = title[title.find('itemprop="name">')+20:]
+		title = title[:title.find('<')-1].strip()
+		year = str(imdbpage)
+		year = year[year.find('<a href="/year/')+17:year.find('<a href="/year/')+21]
+		rating = str(imdbpage)
+		rating = rating[rating.find('ratingValue">')+13:]
+		rating = rating[:rating.find('<')-1]
+	print HTMLParser.HTMLParser().unescape(title + " ("+year+") "+ rating)
+	return HTMLParser.HTMLParser().unescape(title + " ("+year+") "+ rating)
 
 def makeLinkTo(item, path):
 	if not os.path.lexists(path):
@@ -53,7 +83,6 @@ def makeGenreFolderFrom(path, genrepath='./FilmsByGenre'):
 	if not os.path.exists(genrepath):
 		os.makedirs(genrepath)
 	for item in os.listdir(path):
-		print item
 		itempath = os.path.join(path, item)
 		if os.path.isdir(itempath):   #The item is a film folder, get details
 			page = getFilmPage(item)
@@ -65,7 +94,7 @@ def makeGenreFolderFrom(path, genrepath='./FilmsByGenre'):
 				makeLinkTo(itempath,os.path.join(genrepath, genre)+"/"+fulltitle)
 
 if __name__ == '__main__':
-	makeGenreFolderFrom('/Users/hurl/Dropbox/Own/Code/IMDBScraper/test')
+	makeGenreFolderFrom('/Volumes/Film/')
 	# film = getFilmPage("The Expendables 2")
 	# genres = getGenres(film)
 	# title = getFullTitle(film)
